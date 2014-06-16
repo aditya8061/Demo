15efeca7e5ed7f5b1fc43a46da7b0b266edebee4
aditya8061 <aditya.bhardwaja@gmail.com>
2014-06-16 14:50:02 -0500

    Revert "KDM Hackathon"
    This reverts commit 847c86d1cffc7d0eb9bb35e94e3e71c11ee5b90c.

--- KDM/Hackathon/Midterm/Source Code/Scripts/txt_csv.py
+++
@@ -1,6 +0,0 @@
-import csv
-import sys
-with open(slump_file, "rb") as infile, open(slump_csv_file, 'wb') as outfile:
-    in_txt = csv.reader(infile, delimiter = ',')
-    out_csv = csv.writer(outfile)
-    out_csv.writerows(in_txt)
